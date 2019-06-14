212ed02ab0bb2002fe544d5f3f29c306283609cf
FeniXb3 <fenix.b3@gmail.com>
2019-06-14 11:36:43 +0200

    Sketch main with placeholder functions

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 def main():
     show_intro()
+    player = create_character()
+    start_game(player)
 
 
 def show_intro():
     print("Welcome to Rogalik!")
+
+
+def create_character():
+    print("=== Character creation ===")
+    player = {
+        "name": "Eisenheim"
+    }
+
+    return player
+
+
+def start_game(character):
+    print("Let's play, {name}!".format(**character))
 
 
 if __name__ == "__main__":
