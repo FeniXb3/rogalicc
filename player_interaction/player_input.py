e6e658724cd3a787d1969e87dcd047fa6a99c13c
FeniXb3 <fenix.b3@gmail.com>
2019-06-27 08:04:05 +0200

    Add fallback to standard input if termios is not supported

--- player_interaction/player_input.py
+++ player_interaction/player_input.py
@@ -2,11 +2,14 @@ import sys, tty, termios
 
 
 def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
     try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+    except termios.error:
+        return input()
