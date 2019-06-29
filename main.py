6ff872ffa3cc04c689e9bac2018b71e040e1286f
FeniXb3 <fenix.b3@gmail.com>
2019-06-29 08:32:34 +0200

    Split main game function into separate functions

--- main.py
+++ main.py
@@ -89,30 +89,43 @@ def add_door_to_level(level_data):
 def start_game(player):
     display.show_screen_and_wait(templates.GAME_START, player)
 
-    level_raw_view = data_loading.load_level()
-    level_data, level_view = data_loading.parse_level_data(level_raw_view)
-    add_key_to_level(level_data)
-    add_door_to_level(level_data)
+    level_data, level_view = setup_level()
     directions = data_loading.setup_directions()
 
     while True:
-        level_actions.place_character(player, level_data)
-        level_actions.refresh_view(level_data, level_view)
-        show_level(level_view)
-        show_inventory(player[fields.INVENTORY])
+        render_updated_game_view(level_data, level_view, player)
 
         key = player_input.getch()
 
         if key in directions:
-            direction = directions[key]
-            target_position = character_actions.calculate_target_position(player[fields.POSITION], direction)
-            target_cell = level_actions.get_cell_at(level_data, target_position)
-            if character_actions.can_interact(player, target_cell):
-                character_actions.interact(player, target_cell, level_data)
-            if character_actions.can_move(player, target_cell):
-                character_actions.move(player, target_position)
+            perform_character_frame(directions, key, level_data, player)
         elif key == "q":
             leave_game()
+
+
+def perform_character_frame(directions, key, level_data, player):
+    direction = directions[key]
+    target_position = character_actions.calculate_target_position(player[fields.POSITION], direction)
+    target_cell = level_actions.get_cell_at(level_data, target_position)
+    if character_actions.can_interact(player, target_cell):
+        character_actions.interact(player, target_cell, level_data)
+    if character_actions.can_move(player, target_cell):
+        character_actions.move(player, target_position)
+
+
+def render_updated_game_view(level_data, level_view, player):
+    level_actions.place_character(player, level_data)
+    level_actions.refresh_view(level_data, level_view)
+    show_level(level_view)
+    show_inventory(player[fields.INVENTORY])
+
+
+def setup_level():
+    level_raw_view = data_loading.load_level()
+    level_data, level_view = data_loading.parse_level_data(level_raw_view)
+    add_key_to_level(level_data)
+    add_door_to_level(level_data)
+    return level_data, level_view
 
 
 if __name__ == "__main__":
