7ca7a8c471fb900d9ade043f6578531cd1510532
FeniXb3 <fenix.b3@gmail.com>
2019-07-04 21:00:58 +0200

    Unlock doors on interaction when holding key instead of removing them

--- character/interactions.py
+++ character/interactions.py
@@ -1,6 +1,7 @@
 from character import character_actions
 from item import item_types
 from level import level_actions
+from obstacle import obstacle_properties
 
 
 def pick_up_item(character, item, level_data):
@@ -11,4 +12,4 @@ def pick_up_item(character, item, level_data):
 def try_opening_door(character, door, level_data):
     key = character_actions.find_in_inventory(character, item_types.KEY)
     if key:
-        level_actions.remove_obstacle(level_data, door)
+        obstacle_properties.set_is_locked(door, False)
